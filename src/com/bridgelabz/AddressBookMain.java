8b549c0cd96c3e0c7123eec1c3ba2723c4065e97
Prince <princek2530@gmail.com>
2022-12-10 14:26:37 +0530

    Added: Ability to add Multiple Address Book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,9 +1,16 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program in AddressBookMain class on Main Branch");
-        AddressBook addressBook = new AddressBook();
-        addressBook.mainMenu();
+        System.out.println("Enter Number of Address Book to Add");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i=1;i<=num;i++){
+            AddressBook addressBook = new AddressBook();
+            addressBook.mainMenu();
+        }
     }
 }
